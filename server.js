654d6d224fb262b81bc358db3870831c5d55c08e
amna0488 <amnasalahudin123@gmail.com>
2022-10-16 22:39:22 +0500

    second commit

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ app.get('/', (req, res) => {
   res.send('Hello')
 })
 
+app.get('/todos', (req, res) => {
+    res.send('A list of items will be returned')
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+    res.send('Posting a Request')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
